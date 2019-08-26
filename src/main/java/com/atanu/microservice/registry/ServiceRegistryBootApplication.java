7e4f0df58570c236aa9efe3885bf8b7da75182a0
Atanu Bhowmick <mail2atanu007@gmail.com>
2019-08-27 00:24:12 +0530

    Eureka server added

--- src/main/java/com/atanu/microservice/registry/ServiceRegistryBootApplication.java
+++ src/main/java/com/atanu/microservice/registry/ServiceRegistryBootApplication.java
@@ -2,12 +2,13 @@ package com.atanu.microservice.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryBootApplication.class, args);
 	}
-
 }
