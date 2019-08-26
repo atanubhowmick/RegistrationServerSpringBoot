fe86727bed57aa59c171ca074e29a98fbdd181b3
Atanu Bhowmick <mail2atanu007@gmail.com>
2019-08-26 21:21:11 +0530

    First commit

---
+++ src/main/java/com/atanu/microservice/registry/ServiceRegistryBootApplication.java
@@ -0,0 +1,13 @@
+package com.atanu.microservice.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryBootApplication.class, args);
+	}
+
+}
