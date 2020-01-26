e89527846f120a13e494558e500a7e99544da72d
Atanu Bhowmick <mail2atanu007@gmail.com>
2020-01-27 02:04:03 +0530

    Updated test

--- src/test/java/com/atanu/microservice/registry/ServiceRegistryBootApplicationTests.java
+++ src/test/java/com/atanu/microservice/registry/ServiceRegistryBootApplicationTests.java
@@ -1,13 +1,8 @@
 package com.atanu.microservice.registry;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceRegistryBootApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
